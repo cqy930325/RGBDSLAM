b5a01ed76a00f9e86c6ded53d7cb7c564d041c41
Qingyu Chen <cqy@umich.edu>
2016-04-07 19:20:30 -0400

    add octomap

--- src/slam.cpp
+++ src/slam.cpp
@@ -16,6 +16,7 @@ using namespace std;
 
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/filters/passthrough.h>
+#include <pcl/registration/icp.h>
 
 #include <g2o/types/slam3d/types_slam3d.h>
 #include <g2o/core/sparse_optimizer.h>
@@ -27,6 +28,10 @@ using namespace std;
 #include <g2o/core/robust_kernel.h>
 #include <g2o/core/robust_kernel_factory.h>
 #include <g2o/core/optimization_algorithm_levenberg.h>
+
+#include <octomap/octomap.h>
+#include <octomap/ColorOcTree.h>
+#include <octomap/math/Pose6D.h>
 
 // 把g2o的定义放到前面
 typedef g2o::BlockSolver_6_3 SlamBlockSolver; 
@@ -45,9 +50,19 @@ CHECK_RESULT checkKeyframes( FRAME& f1, FRAME& f2, g2o::SparseOptimizer& opti, b
 void checkNearbyLoops( vector<FRAME>& frames, FRAME& currFrame, g2o::SparseOptimizer& opti );
 // 随机检测回环
 void checkRandomLoops( vector<FRAME>& frames, FRAME& currFrame, g2o::SparseOptimizer& opti );
-
+void print4x4Matrix (const Eigen::Matrix4d & matrix)
+{
+  printf ("Rotation matrix :\n");
+  printf ("    | %6.3f %6.3f %6.3f | \n", matrix (0, 0), matrix (0, 1), matrix (0, 2));
+  printf ("R = | %6.3f %6.3f %6.3f | \n", matrix (1, 0), matrix (1, 1), matrix (1, 2));
+  printf ("    | %6.3f %6.3f %6.3f | \n", matrix (2, 0), matrix (2, 1), matrix (2, 2));
+  printf ("Translation vector :\n");
+  printf ("t = < %6.3f, %6.3f, %6.3f >\n\n", matrix (0, 3), matrix (1, 3), matrix (2, 3));
+}
 int main( int argc, char** argv )
 {
+    octomap::ColorOcTree tree( 0.01 ); //全局map
+
     // 前面部分和vo是一样的
     ParameterReader pd;
     int startIndex  =   atoi( pd.getData( "start_index" ).c_str() );
@@ -170,11 +185,23 @@ int main( int argc, char** argv )
         pass.filter( *newCloud );
         // 把点云变换后加入全局地图中
         pcl::transformPointCloud( *newCloud, *tmp, pose.matrix() );
+        print4x4Matrix (pose.matrix());
+        octomap::Pointcloud cloud_octo;
+        for (int i = 0;i < tmp->points.size();++i){
+            cloud_octo.push_back( tmp->points[i].x, tmp->points[i].y, tmp->points[i].z);
+        }
+        
+        tree.insertPointCloud( cloud_octo, 
+                octomap::point3d( pose(0,3), pose(1,3), pose(2,3) ) );
+
+        for (int i = 0;i < tmp->points.size();++i)
+            tree.integrateNodeColor( tmp->points[i].x, tmp->points[i].y, tmp->points[i].z, tmp->points[i].r, tmp->points[i].g, tmp->points[i].b);
         *output += *tmp;
         tmp->clear();
         newCloud->clear();
     }
-
+    tree.updateInnerOccupancy();
+    tree.write( "./data/map.ot" );
     voxel.setInputCloud( output );
     voxel.filter( *tmp );
     //存储
@@ -274,9 +301,31 @@ CHECK_RESULT checkKeyframes( FRAME& f1, FRAME& f2, g2o::SparseOptimizer& opti, b
     edge->setInformation( information );
     // 边的估计即是pnp求解之结果
     Eigen::Isometry3d T = cvMat2Eigen( result.rvec, result.tvec );
-    edge->setMeasurement( T.inverse() );
-    // 将此边加入图中
+    print4x4Matrix(T.matrix());
+    /*PointCloud::Ptr f1c = image2PointCloud( f1.rgb, f1.depth, camera );
+    PointCloud::Ptr f2c = image2PointCloud( f2.rgb, f2.depth, camera );
+    PointCloud::Ptr out(new PointCloud());
+    pcl::IterativeClosestPoint<PointT, PointT> icp;
+    icp.setMaximumIterations(25);
+    icp.setInputSource(f1c);
+    icp.setInputTarget(f2c);
+    icp.align(*out);
+    if(icp.hasConverged()){
+        Eigen::Isometry3d T;
+        Eigen::Matrix4d AT = icp.getFinalTransformation().cast<double>();
+        for(int i=0; i<4;++i){
+            for(int j=0; j<4; ++j){
+                T(i,j) = AT(i,j);
+            }
+        }
+        edge->setMeasurement(T.inverse());
+        opti.addEdge(edge);
+        return KEYFRAME;
+    }*/
+
+    edge->setMeasurement( T);
     opti.addEdge(edge);
+    // 将此边加入图中
     return KEYFRAME;
 }
 
